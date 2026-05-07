e33772325aea71392677fcc52d32e117421be305
hjovanov679-dotcom <hjovanov679@gmail.com>
2026-05-07 11:09:08 +0200

    Update Danny.py

--- Danny.py
+++ Danny.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python3
+from flask import Flask, request, render_template
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/signup", methods=["POST"])
 def signup():
     name = request.form["name"]
-    name = request.form["email"]
-    name = request.form["Date"]
-    name = request.form["Persons"]
+    email = request.form["email"]
+    Date = request.form["Date"]
+    Persons = request.form["Persons"]
 
-def data(name = input(name_signup), email = input(email_signup), date = input(date_signup), persons = input(persons_signup)):
-    send_email(email, name, Date, Persons)
+def send_email(email, name, Date, Persons):
+    sender = os.environ["EMAIL_USER"]
+    password = os.environ["EMAIL_PASS"]
 
-def send_email(email, name):
-    sender = hjovanov679@gmail.com
-    password = tzma mcwv pksk xurk
+    email_data = MIMEMultipart()
+    email_data["From"] = sender
+    email_data["To"] = email
+    email_data["Subject"] = "Bevestiging reservering"
 
-email_data = MIMEMultipart
-body = "Hallo Mr ", name, "Dankuwel voor uw reservering op ", Date, "voor", Persons, "Persons. We kijken er naaruit u te zien.!"
-email_data.attach(MIMEtext(body, plain))
+    body = "Hallo Mr ", name, "Dankuwel voor uw reservering op ", Date, "voor", Persons, "Persons. We kijken er naaruit u te zien.!"
+    email_data.attach(MIMEtext(body, "plain"))
 
-with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-    server.login(sender, password)
-    server.sendmail(sender, email, email_data.as_string())
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+      server.login(sender, password)
+      server.sendmail(sender, email, email_data.as_string())
+
+if __name__ == "__main__":
+    app.run(debug=True)
