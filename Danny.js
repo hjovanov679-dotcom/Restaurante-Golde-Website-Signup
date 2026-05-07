c0193420b1f4048d6d8a7abb4053ff9fa3b67f78
hjovanov679-dotcom <hjovanov679@gmail.com>
2026-05-07 10:40:51 +0200

    Javascript Signup

---
+++ Danny.js
@@ -0,0 +1,7 @@
+const sbu = getElementById("sbu");
+sbu.addEventListener("Click"); {
+    text.style.display = "block"
+}
+sbu.addEventListener("Click"); {
+    console.log("Dankjewel voor je reservering!")
+}
